9d9a233e0a700a89cb6c5fad0416213706c0c6ad
Echoxyr <igormartins6@icloud.com>
2025-09-13 13:46:47 +0200

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,18 @@
-
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontSize: {
+        'touch': ['18px', '1.4'],
+      },
+      spacing: {
+        'touch': '44px',
+      }
+    },
+  },
+  plugins: [],
+}
