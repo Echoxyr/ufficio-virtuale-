fcd0869fe44010dc835e54ab4184d726816a807a
Echoxyr <igormartins6@icloud.com>
2025-09-13 13:56:15 +0200

    useRealtime.ts

--- web/src/hooks/useRealtime.ts
+++ web/src/hooks/useRealtime.ts
@@ -1,1 +1,21 @@
+import { useEffect } from 'react'
+import { supabase } from '../lib/supabase'
 
+export const useRealtime = (table: string, callback: () => void) => {
+  useEffect(() => {
+    const subscription = supabase
+      .channel(`${table}_changes`)
+      .on(
+        'postgres_changes',
+        { event: '*', schema: 'public', table },
+        () => {
+          callback()
+        }
+      )
+      .subscribe()
+
+    return () => {
+      subscription.unsubscribe()
+    }
+  }, [table, callback])
+}
